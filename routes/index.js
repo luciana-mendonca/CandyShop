a9a12dcc2c888420365274840a770cdf0ce3ab2d
Luci-Lawless <lu.gmmm@gmail.com>
2018-08-16 14:06:33 +0200

    Add csurf token middleware

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
+var csrf = require('csurf');
 
+var csrfProtection = csrf();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Product.find(function(err, docs){
