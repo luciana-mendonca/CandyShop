c0247ad7c384979ef16bc1867c064b8f911dd1bc
Luci-Lawless <lu.gmmm@gmail.com>
2018-08-16 19:16:19 +0200

    Create sign in page

--- config/passport.js
+++ config/passport.js
@@ -12,6 +12,7 @@ passport.deserializeUser(function(id, done){
   })
 });
 
+// Sign up
 passport.use('local-signup', new LocalStrategy({
   usernameField: 'email',
   passwordField: 'password',
@@ -27,7 +28,6 @@ passport.use('local-signup', new LocalStrategy({
       });
       return done(null, false, req.flash('error', messages));
     }
-
     User.findOne({'email': email}, function(err, user) {
       if (err) {
         return done(err);
@@ -44,5 +44,35 @@ passport.use('local-signup', new LocalStrategy({
           }
           return done(null, newUser);
         });
+    });
+}));
+
+// Sign in
+passport.use('local-signin', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, function(req, email, password, done) {
+    req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+    req.checkBody('password', 'Invalid password').notEmpty();
+    var errors = req.validationErrors();
+    if(errors) {
+      var messages = [];
+        errors.forEach(function(error) {
+          messages.push(error.msg);
+        });
+      return done(null, false, req.flash('error', messages));
+    }
+    User.findOne({'email': email}, function(err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, {message: 'User not found!'});
+      }
+      if(!user.validPassword(password)) {
+        return done(null, false, {message: 'Incorrect password.'});
+      }
+      return done(null, user);
     });
 }));
