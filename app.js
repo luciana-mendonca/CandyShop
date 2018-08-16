586ac09d5a7bef5bdb81e19919a33f48258a8e90
Luci-Lawless <lu.gmmm@gmail.com>
2018-08-16 16:21:31 +0200

    Create encrypted passwords

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose');
 var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 
@@ -21,6 +23,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({secret: 'mysecret', resave: false, saveUnitialized: false }));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
