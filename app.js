42806d22de3d7472d16852779282d031859d6775
Luci-Lawless <lu.gmmm@gmail.com>
2018-08-17 15:21:01 +0200

    Implement router protection

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var passport = require('passport');
 var flash = require('connect-flash');
 var validator = require('express-validator');
 
-var indexRouter = require('./routes/index');
-
+var routes = require('./routes/index');
+var userRoutes = require('./routes/user');
 
 var app = express();
 mongoose.connect('mongodb://localhost:27017/shopping');
@@ -31,8 +31,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/user', userRoutes);
-app.use('/', indexRouter);
+app.use('/user', userRoutes);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
