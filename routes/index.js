68811a06492c03ef2be6d743f2619ea98a4abf8d
Luci-Lawless <lu.gmmm@gmail.com>
2018-08-29 16:13:50 +0200

    Make user sign in to checkout. Protect routes.

--- routes/index.js
+++ routes/index.js
@@ -40,7 +40,7 @@ router.get('/shopping-cart', function(req, res, next) {
   res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice });
 });
 
-router.get('/checkout', function(req, res, next) {
+router.get('/checkout', isLoggedIn, function(req, res, next) {
   if (!req.session.cart) {
     return res.redirect('/shopping-cart');
   }
@@ -49,7 +49,7 @@ router.get('/checkout', function(req, res, next) {
   res.render('shop/checkout', {total: cart.totalPrice, errMsg: errMsg, noError: !errMsg});
 });
 
-router.post('/checkout', function(req, res, next){
+router.post('/checkout', isLoggedIn, function(req, res, next){
   if (!req.session.cart) {
     return res.redirect('/shopping-cart');
   }
@@ -83,3 +83,10 @@ router.post('/checkout', function(req, res, next){
 });
 
 module.exports = router;
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/user/signin');
+}
