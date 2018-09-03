9fd116652f14f2a56cbcd4bfa24b94f490b1f82e
Luci-Lawless <lu.gmmm@gmail.com>
2018-09-03 11:17:59 +0200

    Delete item information from shopping cart

--- models/cart.js
+++ models/cart.js
@@ -19,6 +19,10 @@ module.exports = function Cart(oldCart) {
     this.items[id].price -= this.items[id].item.price;
     this.totalQty--;
     this.totalPrice -= this.items[id].item.price;
+
+    if(this.items[id].qty <= 0) {
+      delete this.items[id];
+    }
   }
   this.generateArray = function() {
     var arr = [];
